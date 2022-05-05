0f758fb7b76adc8a6b780dfe7606f9646a959294
Jean-Philippe Venant <92146531+JPVenant@users.noreply.github.com>
2022-05-05 22:32:44 +0200

    Add files via upload

--- contact/contact.sql
+++ contact/contact.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.6
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 08 jan. 2020 à 11:12
--- Version du serveur :  5.7.19
--- Version de PHP :  7.1.9
+-- Hôte : aj3nk.myd.infomaniak.com
+-- Généré le :  jeu. 05 mai 2022 à 22:15
+-- Version du serveur :  10.4.22-MariaDB-1:10.4.22+maria~stretch-log
+-- Version de PHP :  7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `contact_form`
+-- Base de données :  `aj3nk_contact_form`
 --
 
 -- --------------------------------------------------------
@@ -28,14 +28,34 @@ SET time_zone = "+00:00";
 -- Structure de la table `contact`
 --
 
-DROP TABLE IF EXISTS `contact`;
-CREATE TABLE IF NOT EXISTS `contact` (
+CREATE TABLE `contact` (
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `subject` varchar(200) NOT NULL,
-  `message` varchar(500) NOT NULL
+  `message` varchar(500) NOT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
